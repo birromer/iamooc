eec661b56c315b48ce139ccfc020c43e1e88a9e3
birromer <hummescmpa@gmail.com>
2018-12-20 12:14:39 -0200

    added library tests

---
+++ exo0.py
@@ -0,0 +1,14 @@
+from pyibex import *
+from vibes import vibes
+# define the function (x,y) -> sin(x+y)
+f = Function('x', 'y', 'x*cos(x-y)+y')
+# build the Forward / backward separator f(x,y) < 0
+sep = SepFwdBwd(f, [-oo, 0])
+# define the initial domain [-10,10]x[-10,10]
+X0 = IntervalVector(2, [-10, 10] )
+#initialize vibes display
+vibes.beginDrawing()
+# run the set inversion algorithm
+pySIVIA(X0, sep, 0.1)
+vibes.saveImage('intervals.jpg')
+vibes.endDrawing()
