892cd93eb883581edd042eab55d40bbda2ea6a65
birromer <hummescmpa@gmail.com>
2018-12-20 12:14:52 -0200

    test file

---
+++ install-with-pip.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 15 15:02:31 2018
+
+@author: birromer
+"""
+
+import subprocess
+import sys
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'vibes'])
