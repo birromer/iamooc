931533ed8946799a1a408f18714dab82b2c91846
birromer <hummescmpa@gmail.com>
2018-12-26 14:41:13 -0200

    added last exo8 script

--- pyibexring3.py
+++ pyibexring3.py
@@ -1,8 +1,18 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 26 14:33:36 2018
+from vibes import *
+from pyibex import *
+import pyibex
 
-@author: birromer
-"""
+vibes.beginDrawing()
+vibes.newFigure('one ring with pyibex')
+vibes.setFigureProperties({'x':200, 'y':100, 'width':800, 'height':800})
+X0=IntervalVector([[-10,10], [-10,10]])
 
+f1 = Function("x[2]", "(x[0]-1)^2+(x[1]-2)^2")
+f2 = Function("x[2]", "(x[0]-2)^2+(x[1]-5)^2")
+
+sep1 = SepFwdBwd(f1, sqr(Interval(4,5)))
+sep2 = SepFwdBwd(f2, sqr(Interval(5,6)))
+#sep=sep1&sep2
+sep=sep1|sep2
+pySIVIA(X0,sep,0.1)
+vibes.axisEqual()
